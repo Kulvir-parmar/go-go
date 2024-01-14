4414f4fdb015a48a431c703919d137de6518f188
Kulvir Singh <ksingh11_be20@thapar.edu>
2024-01-14 11:37:03 +0530

    damn oh damn

--- hello-in-go/hello.go
+++ hello-in-go/hello.go
@@ -2,13 +2,31 @@ package main
 
 import "fmt"
 
-func Hello(name string) string{
+const (
+    spanish = "Hola, "
+    frenchka14 = "Bonjour, "
+)
+
+// defining the output variable creates a varibale in function scope for us.
+func getLanguageHello(language string) (prefix string) {
+    if language == "spanish" {
+        prefix = spanish
+    } else if language == "french" {
+        prefix = frenchka14
+    } else {
+        prefix = "Hello, "
+    }
+
+    return
+}
+
+func Hello(name string, language string) string{
     if name == "" {
         name = "world"
     }
-    return "Hello, " + name
+    return getLanguageHello(language) + name
 }
 
 func main() {
-    fmt.Println(Hello("JJ"));
+    fmt.Println(Hello("JJ", "french"));
 }
--- hello-in-go/hello_test.go
+++ hello-in-go/hello_test.go
@@ -10,14 +10,26 @@ func assertCorrectMessage(t testing.TB, got, want string) {
 }
 
 func TestHello(t* testing.T) {
-    t.Run("saying hello to JJ", func(t* testing.T) {
-        got := Hello("JJ")
+    t.Run("saying hello to JJ in spanish", func(t* testing.T) {
+        got := Hello("JJ", "spanish")
+        want := "Hola, JJ"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("saying hello to JJ in french", func(t* testing.T) {
+        got := Hello("JJ", "french")
+        want := "Bonjour, JJ"
+        assertCorrectMessage(t, got, want)
+    })
+
+    t.Run("saying hello to JJ in deafult lang", func(t* testing.T) {
+        got := Hello("JJ", "")
         want := "Hello, JJ"
         assertCorrectMessage(t, got, want)
     })
 
     t.Run("saying generic hello to all", func(t* testing.T) {
-        got := Hello("")
+        got := Hello("", "")
         want := "Hello, world"
         assertCorrectMessage(t, got, want)
     })
