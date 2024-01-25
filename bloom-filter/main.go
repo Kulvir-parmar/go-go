8658f7b9a53ce5aff9a15d812586431b24131e12
Kulvir Singh <ksingh11_be20@thapar.edu>
2024-01-26 01:53:06 +0530

    false positives percentage

--- bloom-filter/main.go
+++ bloom-filter/main.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 
 	"github.com/twmb/murmur3"
+    "github.com/google/uuid"
 )
 
-var hasher = murmur3.New32()
+var hasher = murmur3.SeedNew32(10)
 
 /*
 Murmur Hash function is used to hash the values
@@ -25,7 +26,7 @@ func mHash(key string, size int32) int32 {
 }
 
 type BloomFilter struct {
-    // TODO: optimize storage to 1 bit for each key
+	// TODO: optimize storage to 1 bit for each key
 	filter []bool
 	size   int32
 }
@@ -37,9 +38,16 @@ func newBloomFilter(size int32) *BloomFilter {
 	}
 }
 
+func (bf *BloomFilter) PrintBloomFilter() {
+	fmt.Println(bf.filter)
+}
+
 func (bf *BloomFilter) Add(key string) {
 	index := mHash(key, bf.size)
 	bf.filter[index] = true
+
+	// NOTE: Just to check the collisions
+	// fmt.Printf("Added at index: %d\n", index)
 }
 
 func (bf *BloomFilter) Exists(key string) bool {
@@ -48,14 +56,38 @@ func (bf *BloomFilter) Exists(key string) bool {
 }
 
 func main() {
-	filter := newBloomFilter(16)
+    filterSize := 1000
 
-	keys := []string{"foo", "bar", "ez", "gg"}
-	for _, key := range keys {
-		filter.Add(key)
-	}
-    
-    fmt.Println(filter.Exists("abc"))
-    fmt.Println(filter.Exists("bar"))
-    fmt.Println(filter.Exists("jj"))
+    dataset := make([]string, 0)
+    existMap := make(map[string]bool)
+    notExistMap := make(map[string]bool)
+
+    for idx := 0; idx < filterSize/2; idx++{
+        id := uuid.New()
+        dataset = append(dataset, id.String())
+        existMap[id.String()] = true
+    }
+
+    for idx := 0; idx < filterSize/2; idx++{
+        id := uuid.New()
+        dataset = append(dataset, id.String())
+        notExistMap[id.String()] = true
+    }
+
+    for j := 500; j < 2000; j+=100 {
+        filter := newBloomFilter(int32(j))
+
+        for id := range existMap {
+            filter.Add(id)
+        }
+
+        falsePositive := 0
+        for _, id := range dataset {
+            if filter.Exists(id) && notExistMap[id] {
+                falsePositive++
+            }
+        }
+
+        fmt.Printf("False Positive: %f\n", float64(falsePositive)/float64(len(dataset)))
+    }
 }
