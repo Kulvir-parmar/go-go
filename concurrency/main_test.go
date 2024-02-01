613f48704de6738a2e1f576ccc565ca3930b0bef
Kulvir Singh <ksingh11_be20@thapar.edu>
2024-02-01 05:40:17 +0530

    mutex

--- concurrency/main_test.go
+++ concurrency/main_test.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 	"testing"
 )
 
-func RaceCondition(t *testing.T) {
+func TestDeadlock(t *testing.T) {
 	var state int32
-	var mu sync.RWMutex
+	mu := sync.Mutex{}
 
 	for i := 0; i < 10; i++ {
 		go func(i int) {
+			fmt.Println(i)
 			mu.Lock()
 			state += int32(i)
 			mu.Unlock()
